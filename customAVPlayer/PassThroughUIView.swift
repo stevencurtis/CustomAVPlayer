f86a7e04687173d6cd4abd7087eb195719a2abe7
Steven Curtis <stevenpcurtis@hotmail.com>
2017-06-08 10:55:17 +0800

    first commit

---
+++ customAVPlayer/PassThroughUIView.swift
@@ -0,0 +1,21 @@
+//
+//  PassThroughUIView.swift
+//  customAVPlayer
+//
+//  Created by Steven Curtis on 05/06/2017.
+//  Copyright © 2017 Steven Curtis. All rights reserved.
+//
+
+import UIKit
+
+class PassThroughUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
